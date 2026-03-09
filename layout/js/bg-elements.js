20a1cb4b9d6f2837e9e7462df2013d0ab079bfa6
Aleksandra Filippova <aleks@Aleksandras-MacBook-Air.local>
2026-03-09 19:01:08 +0300

    add modal window for search

--- layout/js/bg-elements.js
+++ layout/js/bg-elements.js
@@ -1,13 +1,10 @@
+// Set background images for elements ---
 const elements = document.querySelectorAll('.set-bg');
 console.log(elements);
 
-// object elements, method length - has length of the array
 for(let i = 0; i < elements.length; i++) {
-    // const element = elements[i];
-    // const bg = element.getAttribute('data-setbg');
-    // console.log(bg);
-    // element.style.backgroundImage = `url(${bg})`;
-
     console.dir(elements[i]);
-    const src = elements[i].dataset.setbg  // gets data-setbg attribute of element
+    const src = elements[i].dataset.setbg;
+    elements[i].style.backgroundImage = `url(${src})`;
+
 }
--- layout/js/preloader.js
+++ layout/js/preloader.js
@@ -1,1 +1,7 @@
-// console.log(document.querySelector('.preloader')); 
+// Preloader is shown before page is loaded, then removed after 500ms ---
+const preloader = document.querySelector('.preloader');
+preloader.classList.add('active');
+
+setTimeout(() => {
+    preloader.classList.remove('active')
+}, 2500);
